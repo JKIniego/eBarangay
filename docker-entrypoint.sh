e46bf96298e3eff9c468b02ac99609a18bbea030
ali1x3 <avilaalex371@gmail.com>
2026-09-22 12:12:56 +0800

    added dummy env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+# Create an empty .env file if it doesn't exist so key:generate doesn't crash
+if [ ! -f .env ]; then
+    touch .env
+fi
+
 # Generate app key if not set
 if [ -z "$APP_KEY" ]; then
     php artisan key:generate --force
 fi
+
 
 # Run migrations
 php artisan migrate --force
